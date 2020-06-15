6a50749e989964c917410d921aac938c8d40ab93
Tulioh <tulio_hos@hotmail.com>
2020-06-15 18:08:49 -0300

    Adicionando a URL da API remota

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://deltafitgym-api.herokuapp.com/api"
 })
 
 class ApiService{
